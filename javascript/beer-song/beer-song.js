ff36d4a96607d070499c1900380a21b0fa88050b
Damien Coffey <spammed@gmail.com>
2014-10-22 13:58:13 -0400

    Less repetition, and I think more readable.

--- javascript/beer-song/beer-song.js
+++ javascript/beer-song/beer-song.js
@@ -1,33 +1,65 @@
 function Beer() {
 
+    if (!String.format) {
+        String.format = function(format) {
+            var args = Array.prototype.slice.call(arguments, 1);
+            return format.replace(/{(\d+)}/g, function(match, number) {
+                return typeof args[number] != 'undefined'
+                    ? args[number]
+                    : match
+                    ;
+            });
+        };
+    }
+
     return {
-        verse: function (verseNumber) {
-            if (verseNumber === 0 ) {
-                return "No more bottles of beer on the wall, no more bottles of beer.\nGo to the store and buy some more, 99 bottles of beer on the wall.\n";
-            } else if (verseNumber === 1) {
-                return "1 bottle of beer on the wall, 1 bottle of beer.\nTake it down and pass it around, no more bottles of beer on the wall.\n";
-            } else if (verseNumber === 2) {
-                return "2 bottles of beer on the wall, 2 bottles of beer.\nTake one down and pass it around, 1 bottle of beer on the wall.\n";
-            } else {
-                return verseNumber + " bottles of beer on the wall, " + verseNumber + " bottles of beer.\nTake one down and pass it around, " + (verseNumber - 1) + " bottles of beer on the wall.\n";
+
+        printBottleCount : function (count) {
+          switch (count) {
+              case 0 :
+                  return "No more bottles";
+              case 1 :
+                  return "1 bottle";
+              default :
+                  return count + " bottles";
+          }
+        },
+        decrement : function (count) {
+            return (count === 0) ? 99 : count - 1;
+        },
+        direction : function (count) {
+            switch (count) {
+                case 0 :
+                    return "Go to the store and buy some more";
+                case 1 :
+                    return "Take it down and pass it around";
+                default :
+                    return "Take one down and pass it around";
             }
+
         },
 
+        verse: function (verseNumber) {
+            return String.format("{0} of beer on the wall, {1} of beer.\n{2}, {3} of beer on the wall.\n",
+                this.printBottleCount(verseNumber),this.printBottleCount(verseNumber).toLowerCase(),
+                this.direction(verseNumber), this.printBottleCount(this.decrement(verseNumber)).toLowerCase()
+            );
+        },
+
+
         sing : function(start, end) {
-          if (start === 0) {
-              return this.verse(start);
-          } else {
-              if (!end) {
-                  end = 0;
-              }
+            if (start === 0) {
+                return this.verse(start);
+            } else {
+                end = end || 0;
 
-              var song = "";
-              for (var i = start; i >= end; i--) {
-                  song += this.verse(i) + ((i !== end) ? "\n" : "");
-              }
+                var song = "";
+                for (var i = start; i >= end; i--) {
+                    song += this.verse(i) + ((i !== end) ? "\n" : "");
+                }
 
-              return song;
-          }
+                return song;
+            }
         }
 
 
