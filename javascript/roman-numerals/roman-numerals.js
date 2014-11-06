71b9eb4ec31b79cac21e6d42433e14b50d34d2f9
Damien Coffey <spammed@gmail.com>
2014-11-06 14:19:07 -0500

    streamlined using lookup objects in an array

--- javascript/roman-numerals/roman-numerals.js
+++ javascript/roman-numerals/roman-numerals.js
@@ -1,38 +1,29 @@
 function toRoman(number) {
-    var convert = function convert(integer, accumlator) {
-        if (integer === 0) {
-            return accumlator;
-        }
 
-        if (integer >= 1000) {
-            return convert(integer - 1000, accumlator + "M");
-        } else if (integer >= 900) {
-            return convert(integer-900, accumlator + "CM")
-        } else if (integer >= 500) {
-            return convert(integer-500, accumlator + "D")
-        } else if (integer >= 400) {
-            return convert(integer-400, accumlator + "CD")
-        } else if (integer >= 100) {
-            return convert(integer-100, accumlator + "C")
-        } else if (integer >= 90) {
-            return convert(integer-90, accumlator + "XC")
-        } else if (integer >= 50) {
-            return convert(integer-50, accumlator + "L")
-        } else if (integer >= 40) {
-            return convert(integer-40, accumlator + "XL")
-        } else if (integer >= 10) {
-            return convert(integer-10, accumlator + "X")
-        } else if (integer == 9) {
-            return convert(integer-9, accumlator + "IX")
-        } else if (integer >= 5 && integer <= 8) {
-            return convert(integer-5, accumlator + "V")
-        } else if (integer == 4) {
-            return convert(integer-4, accumlator + "IV")
-        } else {
-            return convert(integer-1, accumlator + "I")
-        }
-    }
+    var numeralConversion = [
+        { roman : 'M', numeric: 1000 },
+        { roman : 'CM', numeric: 900 },
+        { roman : 'D', numeric: 500 },
+        { roman : 'CD', numeric: 400 },
+        { roman : 'C', numeric: 100 },
+        { roman : 'XC', numeric: 90 },
+        { roman : 'L', numeric: 50 },
+        { roman : 'XL', numeric: 40 },
+        { roman : 'X', numeric: 10 },
+        { roman : 'IX', numeric: 9 },
+        { roman : 'V', numeric: 5},
+        { roman : 'IV', numeric: 4},
+        { roman : 'I', numeric: 1}
+    ];
 
-    return convert(number, "");
+    var accumulator = "";
+    numeralConversion.forEach(function (conversion) {
+        while (number >= conversion.numeric) {
+            accumulator += conversion.roman;
+            number -= conversion.numeric;
+        }
+    });
+
+    return accumulator;
 }
 module.exports = toRoman;
