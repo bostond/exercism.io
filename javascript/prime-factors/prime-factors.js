bb05f8f619f769b5f446fcfa9830347290af2984
Damien Coffey <spammed@gmail.com>
2014-11-09 16:24:50 -0500

    Fixed 'maximum stack exeeded' error with trampoline strategy.

--- javascript/prime-factors/prime-factors.js
+++ javascript/prime-factors/prime-factors.js
@@ -1,22 +1,45 @@
 var prime = function() {
-
     return {
         for : function(number) {
-            return find(number, 2, []);
+            return  trampoline(function find(number, factor, factors) {
+                if (number === 1 || factor > number) {
+                    return factors;
+                } else if (number % factor === 0) {
+                    factors.push(factor);
+                    return function () {
+                        return find(number / factor, factor, factors);
+                    }
+                } else {
+                    return function () {
+                        return find(number, factor + 1, factors);
+                    }
+                }
+            }, number, 2, []);
         }
     }
 
-    function find(number, factor, factors) {
-        if (number === 1 || factor > number) {
-            return factors;
-        } else if (number % factor === 0) {
-            factors.push(factor);
-            return find(number / factor, factor, factors);
-        } else {
-            return find(number, factor + 1, factors);
+    function trampoline(fun /*, args */) {
+        var result = fun.apply(fun, tail(arguments));
+
+        while (isFunction(result)) {
+            result = result();
         }
-    }
+
+        return result;
+    };
+
+    function tail(array, n, guard) {
+        return Array.prototype.slice.call(array, n == null || guard ? 1 : n);
+    };
+
+    function isFunction(obj) {
+        return Object.prototype.toString.call(obj) === '[object Function]';
+    };
+
+
 
 }
+
+
 
 module.exports = prime();
