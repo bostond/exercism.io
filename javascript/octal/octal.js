4da94b387cfd61ddaf2b484e59464ec69214d90c
Damien Coffey <spammed@gmail.com>
2014-11-19 08:48:18 -0500

    Yawn. Binary, Trinary and now Octal.

---
+++ javascript/octal/octal.js
@@ -0,0 +1,33 @@
+var Octal = function Octal(trinary) {
+    return toInt(trinary, 8);
+}
+
+var toInt = function toInt(number, base){
+    return {
+        toDecimal: function () {
+            try {
+                var digits = number.split("").map(function (digit) {
+                    var int = parseInt(digit, 10);
+                    if (isNaN(int)) {
+                        throw "Not a number: " + digit;
+                    } else {
+                        return int;
+                    }
+                });
+            } catch (e) {
+                return 0;
+            }
+
+            return convert(digits, 1, 0)
+        }
+    }
+
+    function convert (digits, place, accumlator) {
+        if (digits.length === 0) {
+            return accumlator;
+        }
+        return convert(digits, base * place, accumlator + (digits.pop() * place));
+    }
+}
+
+module.exports = Octal;
